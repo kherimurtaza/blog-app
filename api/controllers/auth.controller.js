749bd464d66dc4cf7ffa639b23f2ec1ac3362f8f
kherimurtaza <murtazakheri53@gmail.com>
2024-02-25 10:27:23 +0530

    create signin backend

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-
+import jwt from "jsonwebtoken";
+//sign-Up
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
@@ -21,6 +22,39 @@ export const signUp = async (req, res, next) => {
   try {
     await newUser.save();
     res.json({ message: "Signup Successful" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//sign-In
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email == "" || password == "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "wrong credentials"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "wrong credentials"));
+    }
+    const token = jwt.sign(
+      {
+        Id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
